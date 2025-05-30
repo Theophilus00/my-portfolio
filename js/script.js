63e5ee637da74eff1dc981966eaa0bbdf80aca5a
unknown <Theophilus000>
2025-05-30 13:21:41 +0100

    Initial look of the website

---
+++ js/script.js
@@ -0,0 +1,22 @@
+ // Smooth scroll for navigation links
+document.querySelectorAll('a.nav-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    const href = this.getAttribute('href');
+    if (href.startsWith('#')) {
+      e.preventDefault();
+      document.querySelector(href).scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+// Navbar shadow on scroll
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 30) {
+    navbar.classList.add('shadow');
+  } else {
+    navbar.classList.remove('shadow');
+  }
+});
